f0e06077db97ecbab0fa344a35441809e83e077c
Arthur.Whtoo <whtoo@qq.com>
2021-04-21 09:21:37 +0800

    1. add search bar.

---
+++ src/components/TodoFilter.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="todo-filter">
+    <button type="button" :class="{active : selectedID === 0,inactive : selectedID !== 0}" @click="submit('all',0)" >All</button>
+    <button type="button" :class="{active : selectedID === 1,inactive : selectedID !== 1}" @click="submit('active',1)" >Active</button>
+    <button type="button" :class="{active : selectedID === 2,inactive : selectedID !== 2}" @click="submit('completed',2)">Completed</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TodoFilter",
+  props: {
+  },
+  data() {
+    return {
+       selectedID: 0
+    };
+  },
+  methods: {
+      submit: function(key,id) {
+          this.selectedID = id;
+          this.$store.changeFilterCondition(key);
+      }
+  },
+ 
+  
+};
+</script>
+
+<style scoped>
+button.active {
+    background-color:lightseagreen;
+}
+
+button.inactive {
+    background-color: #94e6db;
+}
+
+</style>
---
+++ src/components/TodoSearchBar.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="todo-searchbar">
+        <input type="text" v-model="searchKey" placeholder="input your search key" @keyup.enter="submit"/>
+        <h4> {{ searchKey }} </h4>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'todo-searchbar',
+    data: function (){
+        return {
+            searchKey: null,
+        }
+    },
+    methods : {
+        submit : function() {
+            this.$store.searchByKeyWords(this.searchKey);
+
+        }
+    }
+}
+</script>
+
+<style scoped>
+div.todo-searchbar input{
+    border: 2px solid rgb(64, 150, 117);
+}
+</style>
